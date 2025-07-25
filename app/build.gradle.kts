4cb4f4a0d2f8a1ee15dbab18bcf442329b0941c0
4566sx <4566sx@users.noreply.github.com>
2025-07-25 09:26:16 +0800

    修复KAPT编译错误：添加缺失的WorkManager Hilt集成依赖

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -95,6 +95,8 @@ dependencies {
     implementation("com.google.dagger:hilt-android:2.44")
     kapt("com.google.dagger:hilt-compiler:2.44")
     implementation("androidx.hilt:hilt-navigation-compose:1.0.0")
+    implementation("androidx.hilt:hilt-work:1.0.0")
+    kapt("androidx.hilt:hilt-compiler:1.0.0")
     
     // Media Player
     implementation("androidx.media3:media3-exoplayer:1.2.1")
